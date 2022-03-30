14add62b01f0472dc5ad2ddb9e99ec22ad8e6678
mathsketch <ktonux@outlook.com>
2022-03-30 22:53:04 +0800

    fix lf icon

--- dot_config/lf/bin/executable_lf-ueberzug.sh
+++ dot_config/lf/bin/executable_lf-ueberzug.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 set -e
-[[ -n "$DISPLAY" ]] || { /usr/bin/lf "$@";exit 0; }
 
 export CONFIG_BIN="$HOME/.config/lf/bin"
 export LF_CACHE="$HOME/.cache/lf"
@@ -11,6 +10,9 @@ cleanup() {
     exec 3>&-
     rm -f "$FIFO_UEBERZUG"
 }
+
+. "$CONFIG_BIN"/iconset.sh
+[[ -n "$DISPLAY" ]] || { /usr/bin/lf "$@";exit 0; }
 
 if ! [[ -d "$LF_CACHE" ]];then
     mkdir -p "$LF_CACHE"
@@ -22,6 +24,5 @@ exec 3>"$FIFO_UEBERZUG"
 
 trap cleanup EXIT
 
-source "$CONFIG_BIN"/iconset.sh
 LC_ALL=en_US.UTF-8 /usr/bin/lf "$@" 3>&-
 "$CONFIG_BIN"/clean.sh
